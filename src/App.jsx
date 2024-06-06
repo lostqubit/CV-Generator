c243f0a0939b089cc18eb2dbed2195d1b3488836
lostqubit <sg_suryaprakash@yahoo.com>
2024-06-06 22:03:26 +0530

    setup basic page layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,26 @@ import "./App.css";
 function App() {
 	return (
 		<>
-			<h2>CV Generator</h2>
+			<InfoForm />
+			<Preview />
 		</>
+	);
+}
+
+function InfoForm() {
+	return (
+		<div className="information">
+			<h1 style={{ textAlign: "center" }}>CV Generator</h1>
+			<p>Create your CV by filling out the fields below! Your CV will be dynamically updated in the preview.</p>
+		</div>
+	);
+}
+
+function Preview() {
+	return (
+		<div className="preview">
+			<div className="cv"></div>
+		</div>
 	);
 }
 

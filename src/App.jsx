b7454e0b897d4bd3a92105264e5e2d6e3be431e8
lostqubit <sg_suryaprakash@yahoo.com>
2024-06-09 22:24:17 +0530

    add work experience component

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,26 @@ const defaultCV = {
 			location: "Roorkee, IN",
 		},
 	],
+	experience: [
+		{
+			id: crypto.randomUUID(),
+			title: "Software Development Engineer",
+			company: "Amazon",
+			"start-date": "",
+			"end-date": "",
+			location: "Bangalore, IN",
+			description: "test-1",
+		},
+		{
+			id: crypto.randomUUID(),
+			title: "DevOps Engineer",
+			company: "Zomato",
+			"start-date": "",
+			"end-date": "",
+			location: "Mumbai, IN",
+			description: "test-2",
+		},
+	],
 };
 
 function App() {
@@ -38,6 +58,7 @@ function InfoForm({ data, setData }) {
 			<Header />
 			<PersonalDetails data={data} setData={setData} />
 			<EducationDetails data={data} setData={setData} />
+			<WorkDetails data={data} setData={setData} />
 		</div>
 	);
 }
@@ -290,6 +311,214 @@ function AddEducationForm({ id, setShowEducationForm, data, setData }) {
 	);
 }
 
+function WorkDetails({ data, setData }) {
+	const [showWorkExForm, setShowWorkExForm] = useState(0);
+
+	return (
+		<div className="work-details">
+			<h2>Work Experience</h2>
+			{showWorkExForm ? (
+				<AddWorkExForm
+					id={showWorkExForm}
+					setShowWorkExForm={setShowWorkExForm}
+					data={data}
+					setData={setData}
+				/>
+			) : (
+				<ViewWorkEx setShowWorkExForm={setShowWorkExForm} data={data} setData={setData} />
+			)}
+		</div>
+	);
+}
+
+function ViewWorkEx({ data, setData, setShowWorkExForm }) {
+	return (
+		<>
+			{data.experience.map((experienceItem) => {
+				return (
+					<WorkExItem
+						id={experienceItem.id}
+						key={experienceItem.id}
+						data={data}
+						setData={setData}
+						setShowWorkExForm={setShowWorkExForm}
+						experience={experienceItem}
+					/>
+				);
+			})}
+			<div className="addExperience">
+				<button
+					className="addExperience-btn"
+					onClick={() => {
+						setShowWorkExForm(1);
+					}}
+				>
+					<img src={addIcon} /> <span>Add Experience</span>
+				</button>
+			</div>
+		</>
+	);
+}
+
+function WorkExItem({ id, data, setData, setShowWorkExForm, experience }) {
+	return (
+		<div className="experience-item">
+			<div>
+				<p>{experience.title}</p>
+				<p>{experience.company}</p>
+			</div>
+			<div>
+				<img src={editIcon} onClick={() => setShowWorkExForm(id)} />
+				<img
+					src={deleteOutlineIcon}
+					onClick={() =>
+						setData({
+							...data,
+							experience: data.experience.filter((experienceItem) => experienceItem.id !== id),
+						})
+					}
+				/>
+			</div>
+		</div>
+	);
+}
+
+function AddWorkExForm({ id, setShowWorkExForm, data, setData }) {
+	const formValue = {
+		title: "",
+		company: "",
+		"start-date": "",
+		"end-date": "",
+		location: "",
+		description: "",
+	};
+
+	if (id !== 1) {
+		const dataItem = data.experience.filter((experienceItem) => experienceItem.id === id)[0];
+		formValue.title = dataItem.title;
+		formValue.company = dataItem.company;
+		formValue["start-date"] = dataItem["start-date"];
+		formValue["end-date"] = dataItem["end-date"];
+		formValue.location = dataItem.location;
+		formValue.description = dataItem.description;
+	}
+
+	return (
+		<>
+			<form
+				id="experience-form"
+				onSubmit={(event) => {
+					event.preventDefault();
+					const formdata = new FormData(event.target);
+					if (id === 1) {
+						const newExperienceItem = {
+							id: crypto.randomUUID(),
+							title: formdata.get("title"),
+							company: formdata.get("company"),
+							"start-date": formdata.get("start-date"),
+							"end-date": formdata.get("end-date"),
+							location: formdata.get("location"),
+							description: formdata.get("description"),
+						};
+						const newData = { ...data };
+						newData.experience.push(newExperienceItem);
+						setData(newData);
+					} else {
+						const editedData = data.experience.map((experienceItem) => {
+							return experienceItem.id === id
+								? {
+										id,
+										title: formdata.get("title"),
+										company: formdata.get("company"),
+										"start-date": formdata.get("start-date"),
+										"end-date": formdata.get("end-date"),
+										location: formdata.get("location"),
+										description: formdata.get("description"),
+								  }
+								: experienceItem;
+						});
+						const newData = { ...data };
+						newData.experience = editedData;
+						setData(newData);
+					}
+					setShowWorkExForm(0);
+					event.target.reset();
+				}}
+			>
+				<InputField
+					type="text"
+					startingValue={formValue.title}
+					title="Position Title"
+					id="title"
+					placeholder="Enter role / position"
+				/>
+				<InputField
+					type="text"
+					startingValue={formValue.company}
+					title="Company"
+					id="company"
+					placeholder="Enter Company / Organization"
+				/>
+				<div className="date">
+					<InputField
+						type="date"
+						startingValue={formValue["start-date"]}
+						title="Start Date"
+						id="start-date"
+					/>
+					<InputField
+						required={false}
+						type="date"
+						startingValue={formValue["end-date"]}
+						title="End Date"
+						id="end-date"
+					/>
+				</div>
+				<InputField
+					required={false}
+					type="text"
+					startingValue={formValue.location}
+					title="Location"
+					id="location"
+					placeholder="Enter Location"
+				/>
+				<InputField
+					required={false}
+					type="textarea"
+					startingValue={formValue.description}
+					title="Description"
+					id="description"
+					placeholder="Enter Description"
+					maxLength="150"
+				/>
+			</form>
+			<div className="buttons">
+				<button
+					className="delete"
+					onClick={() => {
+						if (id !== 1)
+							setData({
+								...data,
+								experience: data.experience.filter((experienceItem) => experienceItem.id !== id),
+							});
+						setShowWorkExForm(0);
+					}}
+				>
+					Delete
+				</button>
+				<div className="cancel-save">
+					<button className="cancel" onClick={() => setShowWorkExForm(0)}>
+						Cancel
+					</button>
+					<button form="experience-form" className="save">
+						Save
+					</button>
+				</div>
+			</div>
+		</>
+	);
+}
+
 function InputField({ required = true, startingValue = "", type, title, id, placeholder, maxLength, data, setData }) {
 	const [value, setValue] = useState(startingValue);
 	if (setData) {
@@ -315,16 +544,27 @@ function InputField({ required = true, startingValue = "", type, title, id, plac
 		return (
 			<div className="input-field">
 				<label htmlFor={id}>{title}</label>
-				<input
-					required={required}
-					onChange={(event) => setValue(event.target.value)}
-					type={type}
-					placeholder={placeholder}
-					id={id}
-					name={id}
-					maxLength={maxLength}
-					value={value}
-				/>
+				{type === "textarea" ? (
+					<textarea
+						onChange={(event) => setValue(event.target.value)}
+						placeholder={placeholder}
+						id={id}
+						name={id}
+						maxLength={maxLength}
+						value={value}
+					/>
+				) : (
+					<input
+						required={required}
+						onChange={(event) => setValue(event.target.value)}
+						type={type}
+						placeholder={placeholder}
+						id={id}
+						name={id}
+						maxLength={maxLength}
+						value={value}
+					/>
+				)}
 			</div>
 		);
 	}
@@ -340,6 +580,9 @@ function Preview({ data }) {
 				{data.address}
 				{data.education.map((educationItem) => {
 					return <div key={educationItem.id}>{educationItem.institute}</div>;
+				})}
+				{data.experience.map((experienceItem) => {
+					return <div key={experienceItem.id}>{experienceItem.title}</div>;
 				})}
 			</div>
 		</div>
